aebafcee7a44066c8afc027130bed702e62b7696
gasparnovel <gnovel@cifpfbmoll.eu>
2021-12-01 23:45:42 +0100

    fix

--- Capitulo04/loginSystem/navigation.php
+++ Capitulo04/loginSystem/navigation.php
@@ -1,34 +1,29 @@
-
 <!-- navbar -->
 <div class="navbar navbar-default navbar-static-top" role="navigation">
     <div class="container-fluid">
-
+ 
         <div class="navbar-header">
             <!-- to enable navigation dropdown when viewed in mobile device -->
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
             </button>
-
+ 
             <!-- Change "Your Site" to your site name -->
-
             <a class="navbar-brand" href="<?php echo $home_url; ?>">Your Site</a>
         </div>
-
+ 
         <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
                 <!-- link to the "Cart" page, highlight if current page is cart.php -->
-                <li <?php echo $page_title="Index" ? "Index" : ""; ?>>
-<!--                    <li --><?php //echo $page_title="Index" ? "class='active'" : ""; ?><!-->-->
+                <li <?php echo $page_title=="Index" ? "class='active'" : ""; ?>>
                     <a href="<?php echo $home_url; ?>">Home</a>
                 </li>
             </ul>
-
+ 
             <?php
-
-            // login and logout options will be here
             // check if users / customer was logged in
             // if user was logged in, show "Edit Profile", "Orders" and "Logout" options
             if(isset($_SESSION['logged_in']) && $_SESSION['logged_in']==true && $_SESSION['access_level']=='Customer'){
@@ -45,9 +40,9 @@
                         </ul>
                     </li>
                 </ul>
-                <?php
+            <?php
             }
-
+            
             // if user was not logged in, show the "login" and "register" options
             else{
                 ?>
@@ -57,7 +52,7 @@
                             <span class="glyphicon glyphicon-log-in"></span> Log In
                         </a>
                     </li>
-
+                
                     <li <?php echo $page_title=="Register" ? "class='active'" : ""; ?>>
                         <a href="<?php echo $home_url; ?>register">
                             <span class="glyphicon glyphicon-check"></span> Register
@@ -65,11 +60,11 @@
                     </li>
                 </ul>
                 <?php
-            }
+                }
             ?>
-
+ 
         </div><!--/.nav-collapse -->
-
+ 
     </div>
 </div>
 <!-- /navbar -->
