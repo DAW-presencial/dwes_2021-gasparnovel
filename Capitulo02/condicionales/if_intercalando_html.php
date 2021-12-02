d426caa802cad4580fa02ed3db91265534cd565a
gasparnovel <gnovel@cifpfbmoll.eu>
2021-12-02 14:56:47 +0100

    feat: :sparkles: if_intercalando_html.php

---
+++ Capitulo02/condicionales/if_intercalando_html.php
@@ -0,0 +1,11 @@
+<?php if (false): ?>
+/* Código que permite intercalar html/css/javascript y otros lenguajes fácilmente */
+/* Ya que a través de condicionales se muestra un html u otro */
+
+html code to run if condition is true
+
+<?php else: ?>
+
+html code to run if condition is false
+
+<?php endif ?>
