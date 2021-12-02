8e598882aa25b0f0b3419c6c15df89b0b53ba2e5
gasparnovel <gnovel@cifpfbmoll.eu>
2021-12-02 22:52:04 +0100

    feat: :sparkles: 1er_form_sin_validacion.php

---
+++ Capitulo02/1er_form_sin_validacion.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<!-- Ejemplo de formulario sin validación de campos, 
+-->
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Sin validación</title>
+</head>
+
+<body>
+    <?php
+    // Comprobamos si se reciben datos del forumulario.
+    if (isset($_GET['submit'])) {
+        // recomendable usar la función filter_input() en lugar de $_GET[]
+        $nombre = $_GET['nombre'] ?? '';
+        $edad = $_GET['edad'] ?? '';
+        $email = filter_input(INPUT_GET, 'email', FILTER_VALIDATE_EMAIL);
+        var_dump($_GET);
+        echo "El formulario fue correctamente procesado";
+        // Aquí procesaríamos el formulario.
+    } else { // 
+        displayForm();
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    function DisplayForm()
+    {
+        global $nombre, $edad, $email;
+    ?>
+        <h1>Registro de usuario</h1>
+        <form>
+            <input type="text" name="nombre[]" placeholder="Nombre" value="<?= $nombre; ?>"/>
+            <input type="text" name="nombre[]" placeholder="Edad" value="<?= $edad; ?>" />
+            <input type="text" name="email" placeholder="email" value="<?= $email; ?>" />
+            <input type="submit" value="Enviar" name="submit" />
+        </form>
+    <?php
+    }
+    ?>
+</body>
+
+</html>
