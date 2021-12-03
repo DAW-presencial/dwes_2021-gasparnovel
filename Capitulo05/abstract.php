b319a152d20bdaa51dcc8cd3246701c4722a623e
gasparnovel <gnovel@cifpfbmoll.eu>
2021-12-03 11:04:04 +0100

    feat: :sparkles: abstract.php

---
+++ Capitulo05/abstract.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+Si instanciamos una clase abstracta, nos dara un 'Fatal error'
+ */
+
+abstract class ClaseAbstracta {
+
+    abstract function unmetodo();
+}
+
+abstract class ClaseAbstractaHija extends ClaseAbstracta {
+    
+}
+
+class Nieta extends ClaseAbstractaHija {
+
+    function unmetodo() {
+        echo "Hola Mundo";
+    }
+
+}
+
+$o = new Nieta;
+echo $o->unmetodo();
+
+//nos darís un error
+//$abst= new ClaseAbstractaHija;
+
+?>
