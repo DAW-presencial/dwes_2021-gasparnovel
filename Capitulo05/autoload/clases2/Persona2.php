22fe5269c44622feff4dfd2af24921db0f3058b7
gasparnovel <gnovel@cifpfbmoll.eu>
2021-12-03 02:24:28 +0100

    feat: :sparkles: Persona2.php

---
+++ Capitulo05/autoload/clases2/Persona2.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Description of Persona2
+ *
+ */
+// declare class Persona2
+class Persona2 {
+    // contructor
+    public function __construct() {
+        echo "<br/>Instanciando Persona2";
+    }
+}
